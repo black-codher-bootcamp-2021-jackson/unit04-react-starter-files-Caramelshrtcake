9784538bb2f02cf1847c9167713e0a48f3ffd001
[caramelshrtcake] <[coleyann@ymail.com]>
2021-11-03 20:19:57 +0000

    bookcounter added

--- session04/src/components/BookCounter.js
+++ session04/src/components/BookCounter.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
+import './BookCounter.css';
 const BookCounter = (props) => {
- return (
- <main>
- <h2>{props.library.name}'s 
-Books</h2>
- {props.library.books.map((book) => 
-(<div>{book.title}</div>))}
- </main>
- );
-}
+  return (
+    <main>
+      <h2>{props.library.name}'s Books</h2>
+      <button onClick={() => props.refreshBooks()}>Refresh Books</button>
+      <ul>
+      {props.library.books.map((book) => (
+        <li>{book.title}</li>
+      ))}
+      </ul>
+    </main>
+  );
+};
 export default BookCounter;
